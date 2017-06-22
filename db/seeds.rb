3c40ec32dfbcd6437ecbf4aae319c981fd2d2396
Alan Covarrubias <aecovarru@gmail.com>
2017-06-22 08:06:36 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 module Builder
-  Seasons.create([2017])
-  season = Season.find_by_year(2017)
-  Players.create(season)
-  Games.create(season)
   game_date = GameDate.today
+  Stats.games(game_date)
+  game_date = GameDate.tomorrow
   Stats.games(game_date)
 end
